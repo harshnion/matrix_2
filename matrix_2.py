1492d05cf81a374c987b691f3884fde3abfed71c
harshnion <86431354+harshnion@users.noreply.github.com>
2021-06-28 23:45:02 +0530

    5_commit

---
+++ matrix_2.py
@@ -0,0 +1,14 @@
+# So How's your coding going on 
+#today we will learn something more interesting with help of matrix 
+# we will table using python matrix
+
+#so here is the table which i already converted into list
+m = [ ["loius" , 26 , "HR"], ["aayush",19,"none"]]
+
+#In this we take credentials of each entry in loop so that we print the the whole table
+for i in m:
+  print('Name :' , i[0])
+  print('age :' , i[1])
+  print('Department :' , i[2])
+  print('-'*20)
+ # Read it carefully as it is littlebit confusing
